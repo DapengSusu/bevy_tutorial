e00f802373663d7b5f8f8da23ba0e63778048186
Jumper <911079536@qq.com>
2022-10-24 00:18:31 +0800

    first commit

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use bevy::prelude::*;
+
+pub const CLEAR: Color = Color::rgb(0.1, 0.1, 0.1);
+
+fn main() {
+    App::new()
+        .insert_resource(ClearColor(CLEAR))
+        .insert_resource(WindowDescriptor {
+            width: 480.0,
+            height: 320.0,
+            title: "bevy_tuturial".to_string(),
+            present_mode: bevy::window::PresentMode::AutoNoVsync,
+            resizable: false,
+            ..Default::default()
+        })
+        .add_plugins(DefaultPlugins)
+        .run();
+}
