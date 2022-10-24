e7c31b86e9c5188e30718cce4ee97e53f9f820b2
Jumper <911079536@qq.com>
2022-10-24 23:53:34 +0800

    add camera 2d

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,36 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, render::camera::ScalingMode};
 
 pub const CLEAR: Color = Color::rgb(0.1, 0.1, 0.1);
+pub const RESOLUTION: f32 = 16.0 / 9.0;
 
 fn main() {
+    let height = 900.0;
     App::new()
         .insert_resource(ClearColor(CLEAR))
         .insert_resource(WindowDescriptor {
-            width: 480.0,
-            height: 320.0,
+            width: height * RESOLUTION,
+            height,
             title: "bevy_tuturial".to_string(),
             resizable: false,
             ..Default::default()
         })
+        .add_startup_system(spawn_camera_system)
         .add_plugins(DefaultPlugins)
         .run();
+}
+
+fn spawn_camera_system(mut command: Commands) {
+    let graph_projection = OrthographicProjection {
+        bottom: -1.0,
+        top: 1.0,
+        left: -1.0 * RESOLUTION,
+        right: 1.0 * RESOLUTION,
+        scaling_mode: ScalingMode::None,
+        ..Default::default()
+    };
+
+    command.spawn_bundle(Camera2dBundle {
+        projection: graph_projection,
+        ..Default::default()
+    });
 }
